3891790cab6164917458dfae33a87113c5833e74
blemeill <blemeill@BRITAB.lesmuids.windows>
2024-12-17 17:15:14 +0100

    Phase 8 : ORM (Tri des enregistrements)

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -5,7 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-/*use App\Repository\VisiteRepository;
+use App\Repository\VisiteRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Description of VoyagesController
@@ -14,13 +15,47 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class VoyagesController extends AbstractController{
     
-    #[Route('/voyages', name: 'voyages')]
-    public function index(): Response{
-    $visites = $this->repository->findAll();
-    return $this->render("pages/voyages.html.twig", [
-        'visites' => $visites
-    ]);
-}
+    private $repository;
+    
+    public function __construct(VisiteRepository $repository) {
+        $this->repository = $repository;
+    }
+    
+    #[Route('/voyages', name: 'voyages', methods: ['GET', 'POST'])]
+    public function index(Request $request): Response
+    {
+            $champ = $request->query->get('champ', null);
+            $ordre = $request->query->get("ordre", 'ASC');
+            //$valeur = $request->query->get('recherche', null);
 
+            if (!in_array($ordre, ['ASC', 'DESC'])) {
+                $ordre = 'ASC';
+            }
+            
+            $allowFields = ['ville', 'pays', 'note', 'datecreation'];
+            if (!in_array($champ, $allowFields)) {
+                throw new \InvalidArgumentException("Invali sorting field: $champ");
+            }
+            
+            if ($champ) {
+                    $visites = $this->repository->findAllOrderBy($champ, $ordre);
+            } else {
+                    $visites = $this->repository->findAll();
+            }
+
+            return $this->render('pages/voyages.html.twig', [
+                    'visites' => $visites,
+            ]);
+    }
+
+    #[Route('/voyages/recherche/{champ}', name: 'voyages.findallequal', methods: ['GET'])]
+    public function findAllEqual($champ, Request $request) : Response {
+            $valeur = $request->query->get('recherche');
+            $visites = $this->reposoitory->findByEqualValue($champ,$valeur);
+
+            return $this->render('pages/voyages.html.twig', [
+                    'visites' => $visites,
+            ]);
+    }
 
 }
--- src/Repository/VisiteRepository.php
+++ src/Repository/VisiteRepository.php
@@ -47,8 +47,60 @@ class VisiteRepository extends ServiceEntityRepository
     //        ;
     //    }
     
+    /**
+     * Retourne toutes les visites triées sur un champ
+     * @param type $champ
+     * @param type $ordre
+     * @return Visite[]
+     */
+
+    public function findAllOrderBy(string $champ, string $ordre, ?string $valeur = null): array {
+	$qb = $this->createQueryBuilder('v');
+        
+        $allowFields = ['ville', 'pays', 'note', 'datecreation'];
+        if (!in_array($champ, $allowFields)) {
+            throw new \InvalidArgumentException("Invali sorting field: $champ");
+        }
+
+	if ($valeur) {
+		$qb->where('v.' . $champ . 'LIKE : valeur')
+		   ->setParameter('valeur', '%' . $valeur . '%');
+	}
+        
+        if ($champ === 'note' || $champ === 'datecreation') {
+            $ordre = $ordre === 'ASC' ? 'DESC' : 'ASC';
+        }
+
+	$qb->orderBy('v.' . $champ, $ordre);
+
+	return $qb->getQuery()->getResult();
+
+        
+    }
     
+    /**
+     * Enregistrements dont un champ est égal à une valeur
+     * ou tous les enregistrements si la valeur est vide
+     * @param type $champ
+     * @param type $valeur
+     * @return Visite[]
+     */
     
+    public function findByEqualValue($champ, $valeur) : array {
+        if ($valeur=="") {
+            return $this->createQueryBuilder('v')
+                    ->orderBy('v.' .$champ, 'ASC')
+                    ->getQuery()
+                    ->getResult();
+        } else {
+            return $this->createQueryBuilder('v')
+                    ->where('v.' .$champ.'=:valeur')
+                    ->setParameter('valeur', $valeur)
+                    ->orderBy('v.datecreation', 'DESC')
+                    ->getQuery()
+                    ->getResult();
+        }
+    }
 }
 
 
