20fb65b4627ade504dfc5533339394b1ab9bde1c
blemeill <blemeill@BRITAB.lesmuids.windows>
2024-12-18 19:04:23 +0100

    Phase 14 : ORM (Ajout table et lien entre tables)

--- src/Entity/Visite.php
+++ src/Entity/Visite.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\VisiteRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use DateTime;
@@ -36,9 +38,16 @@ class Visite
 
     #[ORM\Column(nullable: true)]
     private ?int $tempmax = null;
+
+    /**
+     * @var Collection<int, Environnement>
+     */
+    #[ORM\ManyToMany(targetEntity: Environnement::class)]
+    private Collection $environnements;
     
     public function __construct() {
         $this->datecreation = new DateTime('now');
+        $this->environnements = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -135,6 +144,30 @@ class Visite
     public function setTempmax(?int $tempmax): static
     {
         $this->tempmax = $tempmax;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Environnement>
+     */
+    public function getEnvironnements(): Collection
+    {
+        return $this->environnements;
+    }
+
+    public function addEnvironnement(Environnement $environnement): static
+    {
+        if (!$this->environnements->contains($environnement)) {
+            $this->environnements->add($environnement);
+        }
+
+        return $this;
+    }
+
+    public function removeEnvironnement(Environnement $environnement): static
+    {
+        $this->environnements->removeElement($environnement);
 
         return $this;
     }
